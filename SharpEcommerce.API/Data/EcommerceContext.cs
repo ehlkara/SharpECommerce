80659de70532382fcb0a7b0a56fb712c368362a1
ehlkara <ehlullah.fb@gmail.com>
2023-08-03 11:43:43 +0300

    created dbcontext

---
+++ SharpEcommerce.API/Data/EcommerceContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharpEcommerce.API.Entities;
+
+namespace SharpEcommerce.API.Data
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
