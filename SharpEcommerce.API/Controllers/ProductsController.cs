137153c8b3318352a4138fa6851bdb8dc54b7f46
ehlkara <ehlullah.fb@gmail.com>
2023-08-03 11:38:41 +0300

    Created product entity

---
+++ SharpEcommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharpEcommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "this will be a product";
+        }
+    }
+}
