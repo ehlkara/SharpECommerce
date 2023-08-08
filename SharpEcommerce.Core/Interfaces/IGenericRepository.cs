425c6c68d81b196443d21581280459b9b7f299f4
ehlkara <ehlullah.fb@gmail.com>
2023-08-08 15:34:32 +0300

    created specification evaluator

--- SharpEcommerce.Core/Interfaces/IGenericRepository.cs
+++ SharpEcommerce.Core/Interfaces/IGenericRepository.cs
@@ -1,10 +1,13 @@
 ﻿using SharpEcommerce.Core.Entities;
+using SharpEcommerce.Core.Specifications;
 
 namespace SharpEcommerce.Core.Interfaces
 {
     public interface IGenericRepository<T> where T : BaseEntity
     {
         Task<T> GetByIdAsync(int id);
-        Task<IReadOnlyList<T>> ListAllAsync(); 
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
     }
 }
---
+++ SharpEcommerce.Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharpEcommerce.Core.Entities;
+using SharpEcommerce.Core.Specifications;
+
+namespace SharpEcommerce.Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
+            var query = inputQuery.AsQueryable();
+
+            if(spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
