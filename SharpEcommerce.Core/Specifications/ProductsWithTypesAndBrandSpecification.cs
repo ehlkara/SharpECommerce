5764bb54c0fd6189b545310280c2248961ab6b43
ehlkara <ehlullah.fb@gmail.com>
2023-08-18 17:25:24 +0300

    adding search functionality

--- SharpEcommerce.Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
+++ SharpEcommerce.Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
@@ -5,7 +5,8 @@ namespace SharpEcommerce.Core.Specifications
 {
     public class ProductsWithTypesAndBrandSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandSpecification(ProductSpecParams productParams) : base(x => (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
+        public ProductsWithTypesAndBrandSpecification(ProductSpecParams productParams) : base(x =>(string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) && 
+        (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
