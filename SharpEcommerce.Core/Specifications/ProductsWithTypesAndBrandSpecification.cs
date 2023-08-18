937577cc707a4490996280243bf15bf3b8beecc2
ehlkara <ehlullah.fb@gmail.com>
2023-08-18 16:45:46 +0300

    adding filtering functionality

--- SharpEcommerce.Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
+++ SharpEcommerce.Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
@@ -5,7 +5,7 @@ namespace SharpEcommerce.Core.Specifications
 {
     public class ProductsWithTypesAndBrandSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandSpecification(string sort)
+        public ProductsWithTypesAndBrandSpecification(string sort, int? brandId, int? typeId) : base(x => (!brandId.HasValue || x.ProductBrandId == brandId) && (!typeId.HasValue || x.ProductTypeId == typeId))
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
