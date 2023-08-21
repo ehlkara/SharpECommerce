9564bf0fa19b063eae59d2cab7028a7c6e3b166c
ehlkara <ehlullah.fb@gmail.com>
2023-08-21 16:16:32 +0300

    Consumed services in the component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +7,9 @@ import { Pagination } from './shared/models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Sharp';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:7001/api/products?pageSize=50').subscribe({
-      next: response => this.products = response.data, // what to do next
-      error: error => console.log(error), // what to do if there is an error
-      complete: () => {
-        console.log('request completed');
-        console.log('extra statement');
-      }
-    })
   }
 }
