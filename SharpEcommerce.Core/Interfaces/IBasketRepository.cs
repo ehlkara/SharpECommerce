5be8b2f8a2df9a72f3ef72d851d847276bd75bf4
ehlkara <ehlullah.fb@gmail.com>
2023-08-30 15:29:15 +0300

    created basket repository

---
+++ SharpEcommerce.Core/Interfaces/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using SharpEcommerce.Core.Entities;
+
+namespace SharpEcommerce.Core.Interfaces
+{
+    public interface IBasketRepository
+	{
+		Task<CustomerBasket> GetBasketAsync(string basketId);
+		Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+		Task<bool> DeleteBasketAsync(string basketId);
+	}
+}
+
---
+++ SharpEcommerce.Infrastructure/Data/BasketRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SharpEcommerce.Core.Entities;
+using SharpEcommerce.Core.Interfaces;
+
+namespace SharpEcommerce.Infrastructure.Data
+{
+	public class BasketRepository : IBasketRepository
+	{
+		public BasketRepository()
+		{
+		}
+
+        public Task<bool> DeleteBasketAsync(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
