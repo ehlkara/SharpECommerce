3b13a04676092eefd486aaf350459b41a8fdd31a
ehlkara <ehlullah.fb@gmail.com>
2023-09-02 16:58:52 +0300

    setting up the identity classes

---
+++ SharpEcommerce.Core/Entities/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SharpEcommerce.Core.Entities.Identity
+{
+    public class AppUser : IdentityUser
+	{
+		public string DisplayName { get; set; }
+		public Address Address { get; set; }
+	}
+}
+
