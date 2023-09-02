b25fc58e3eb31847ecb0f8ae57e6e6cd2e15c1e6
ehlkara <ehlullah.fb@gmail.com>
2023-09-02 17:48:31 +0300

    added registering a user

---
+++ SharpEcommerce.API/Dtos/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SharpEcommerce.API.Dtos
+{
+	public class RegisterDto
+	{
+		public string DisplayName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
+
