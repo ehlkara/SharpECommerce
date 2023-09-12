bc459f49b8127613ab37e3c96c316801c07319a2
ehlkara <ehlullah.fb@gmail.com>
2023-09-12 17:28:58 +0300

    Configured the order entities

---
+++ SharpEcommerce.Infrastructure/Config/DeliveryMethodConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SharpEcommerce.Core.Entities.OrderAggregate;
+
+namespace SharpEcommerce.Infrastructure.Config
+{
+    public class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.Property(d => d.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ SharpEcommerce.Infrastructure/Config/OrderItemConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SharpEcommerce.Core.Entities.OrderAggregate;
+
+namespace SharpEcommerce.Infrastructure.Config
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.OwnsOne(i => i.ItemOrdered, io => { io.WithOwner(); });
+            builder.Property(i => i.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
