811cd41dafcea299b0d216ded699df2a5b0c76b0
ehlkara <ehlullah.fb@gmail.com>
2023-09-25 15:59:50 +0300

    Shaped the order data

--- SharpEcommerce.API/Helpers/MappingProfile.cs
+++ SharpEcommerce.API/Helpers/MappingProfile.cs
@@ -2,6 +2,7 @@
 using SharpEcommerce.API.Dtos;
 using SharpEcommerce.Core.Entities;
 using SharpEcommerce.Core.Entities.Identity;
+using SharpEcommerce.Core.Entities.OrderAggregate;
 
 namespace SharpEcommerce.API.Helpers
 {
@@ -14,10 +15,12 @@ namespace SharpEcommerce.API.Helpers
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
 
-            CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<Core.Entities.Identity.Address, AddressDto>().ReverseMap();
             CreateMap<CustomerBasket, CustomerBasketDto>().ReverseMap();
             CreateMap<BasketItem, BasketItemDto>().ReverseMap();
             CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>().ReverseMap();
+            CreateMap<Order, OrderToReturnDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
         }
     }
 }
