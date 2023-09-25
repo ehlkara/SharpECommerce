06f846da81281ba4dbee6fae591b503d4960429a
ehlkara <ehlullah.fb@gmail.com>
2023-09-25 16:04:32 +0300

    automapper config for orders

--- SharpEcommerce.API/Helpers/MappingProfile.cs
+++ SharpEcommerce.API/Helpers/MappingProfile.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using SharpEcommerce.API.Dtos;
 using SharpEcommerce.Core.Entities;
-using SharpEcommerce.Core.Entities.Identity;
 using SharpEcommerce.Core.Entities.OrderAggregate;
 
 namespace SharpEcommerce.API.Helpers
@@ -19,8 +18,15 @@ namespace SharpEcommerce.API.Helpers
             CreateMap<CustomerBasket, CustomerBasketDto>().ReverseMap();
             CreateMap<BasketItem, BasketItemDto>().ReverseMap();
             CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>().ReverseMap();
-            CreateMap<Order, OrderToReturnDto>().ReverseMap();
-            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.ShippingPrice, o => o.MapFrom(s => s.DeliveryMethod.Price))
+                .ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered.ProductItemId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered.ProductName))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered.PictureUrl))
+                .ReverseMap();
         }
     }
 }
