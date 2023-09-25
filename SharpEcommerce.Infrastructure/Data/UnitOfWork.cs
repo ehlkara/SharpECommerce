5b33fb70ed85b1e02c90555f71d1b9ea4ac44f99
ehlkara <ehlullah.fb@gmail.com>
2023-09-25 15:23:33 +0300

    nullable column in order table

--- SharpEcommerce.Infrastructure/Data/UnitOfWork.cs
+++ SharpEcommerce.Infrastructure/Data/UnitOfWork.cs
@@ -25,11 +25,11 @@ namespace SharpEcommerce.Infrastructure.Data
 
         public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
         {
-            if(_repositories == null) _repositories = new Hashtable();
+            if (_repositories == null) _repositories = new Hashtable();
 
             var type = typeof(TEntity).Name;
-            
-            if(!_repositories.ContainsKey(type))
+
+            if (!_repositories.ContainsKey(type))
             {
                 var repositoryType = typeof(GenericRepository<>);
                 var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context);
