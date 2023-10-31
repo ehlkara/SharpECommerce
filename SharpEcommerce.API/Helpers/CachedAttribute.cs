babd70dc280a83cb36d5089013bf5e2808faca99
ehlkara <ehlullah.fb@gmail.com>
2023-10-31 11:30:56 +0300

    created cache attribute class

---
+++ SharpEcommerce.API/Helpers/CachedAttribute.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SharpEcommerce.Core.Interfaces;
+using System.Text;
+
+namespace SharpEcommerce.API.Helpers
+{
+    public class CachedAttribute : Attribute, IAsyncActionFilter
+    {
+        private readonly int _timeToLiveSeconds;
+        public CachedAttribute(int timeToLiveSeconds)
+        {
+            _timeToLiveSeconds = timeToLiveSeconds;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var cacheService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
+
+            var cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
+            var cacheResponse = await cacheService.GetCachedResponseAsync(cacheKey);
+
+            if(!string.IsNullOrEmpty(cacheResponse))
+            {
+                var contentResult = new ContentResult
+                {
+                    Content = cacheResponse,
+                    ContentType = "application/json",
+                    StatusCode = 200,
+                };
+
+                context.Result = contentResult;
+                return;
+            }
+
+            var executedContext = await next(); // move to controller
+
+            if(executedContext.Result is ObjectResult okObjectResult)
+            {
+                await cacheService.CacheResponseAsync(cacheKey, okObjectResult.Value, TimeSpan.FromSeconds(_timeToLiveSeconds));
+            }
+        }
+
+        private string GenerateCacheKeyFromRequest(HttpRequest request)
+        {
+            var keyBuilder = new StringBuilder();
+
+            keyBuilder.Append($"{request.Path}");
+
+            foreach (var (key, value) in request.Query.OrderBy(x => x.Key))
+            {
+                keyBuilder.Append($"|{key}-{value}");
+            }
+
+            return keyBuilder.ToString();
+        }
+    }
+}
