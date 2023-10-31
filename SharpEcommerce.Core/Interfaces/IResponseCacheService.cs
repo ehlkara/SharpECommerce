fa0b64ed7206fd3e7eee3a7ee876aa0646e569b9
ehlkara <ehlullah.fb@gmail.com>
2023-10-31 11:40:28 +0300

    tested cache

--- SharpEcommerce.Core/Interfaces/IResponseCacheService.cs
+++ SharpEcommerce.Core/Interfaces/IResponseCacheService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SharpEcommerce.Core.Interfaces
+﻿namespace SharpEcommerce.Core.Interfaces
 {
     public interface IResponseCacheService
     {
